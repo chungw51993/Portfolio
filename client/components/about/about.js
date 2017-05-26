8da67ce1bfdf99f91669f1307a0eebc1b968a0b0
Won Jin Chung <chungw51993@gmail.com>
2017-05-26 14:35:13 -0500

    Added pictures and background

--- client/components/about/about.js
+++ client/components/about/about.js
@@ -1,14 +1,9 @@
 angular.module('portfolio.about', [])
 
-.controller('AboutCtrl', function() {
-
-})
 .directive('about', function() {
   return {
     restrict: 'E',
     scope: {},
-    templateUrl: '/components/about/about.html',
-    controller: 'AboutCtrl',
-    controllerAs: 'info'
+    templateUrl: '/components/about/about.html'
   }
 })
