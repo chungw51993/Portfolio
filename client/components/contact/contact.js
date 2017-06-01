83713fd3b0315f1497c087989ecf4078945990df
Won Jin Chung <chungw51993@gmail.com>
2017-06-01 18:03:34 -0500

    Fixed project styling and added snackbar

--- client/components/contact/contact.js
+++ client/components/contact/contact.js
@@ -14,6 +14,17 @@ angular.module('portfolio.contact', [])
       message_html: this.message
     }
     emailjs.send('jin_sgmail', 'jin_stemplate', message)
+      .then((response) => {
+        console.log('SUCCESSFUL: ', response);
+      }, (error) => {
+        console.error("FAILED TO SEND: ", error);
+      })
+    let snackbarContainer = document.querySelector('#snackbar');
+    let data = {
+      message: 'Thank you for sending me a message!',
+      timeout: 2000,
+    };
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
     this.name = '';
     this.email = '';
     this.subject = '';
